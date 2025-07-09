d993475f9df77fce38c95e185fb26766367215e3
Trieu.Nguyen <quantrieu.sp@gmail.com>
2025-07-09 20:47:51 +0700

    Clean file

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start MLflow UI in background
-mlflow ui --backend-store-uri ./mlruns --host 0.0.0.0 --port 5000 &
-
-# Run Streamlit UI
-streamlit run app.py --server.port 8501 --server.address 0.0.0.0
